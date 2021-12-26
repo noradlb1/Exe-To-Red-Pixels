0db5c8d56bc6c9e43e2a9e8df048e145c11b322b
noradlb1 <zcyfkhuru.bz@mail.ru>
2021-12-26 08:17:54 +0100

    Exe To Red Pixels
    Exe To Red Pixels

---
+++ Exe To Red Pixels/Form1.Designer.vb
@@ -0,0 +1,83 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button1.Location = New System.Drawing.Point(12, 9)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(58, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "EXE"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(76, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(188, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Button2
+        '
+        Me.Button2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.Button2.Location = New System.Drawing.Point(140, 38)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(124, 34)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Save"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(276, 86)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Exe To Red Pixels"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Exe To Red Pixels/Form1.vb
@@ -0,0 +1,59 @@
+﻿Imports System.Drawing.Imaging
+Imports System.IO
+Imports Microsoft.VisualBasic.CompilerServices
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Dim openFileDialog As OpenFileDialog = New OpenFileDialog()
+            openFileDialog.Filter = "(*.exe)|*.exe"
+            openFileDialog.ShowDialog()
+            Dim flag As Boolean = Operators.CompareString(openFileDialog.FileName, "", False) <> 0
+            If flag Then
+                TextBox1.Text = openFileDialog.FileName
+                Me.img = File.ReadAllBytes(openFileDialog.FileName)
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+
+    Public img As Byte()
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Try
+            Dim saveFileDialog As SaveFileDialog = New SaveFileDialog()
+            saveFileDialog.Filter = "(*.png)|*.png"
+            saveFileDialog.FileName = "Image"
+            saveFileDialog.ShowDialog()
+            Dim flag As Boolean = Operators.CompareString(saveFileDialog.FileName, "", False) <> 0
+            If flag Then
+                Dim array As Byte() = Me.img
+                Dim bitmap As Bitmap = New Bitmap(array.Length, 1)
+                Dim arg_50_0 As Integer = 0
+                Dim num As Integer = array.Length - 1
+                Dim num2 As Integer = arg_50_0
+                While True
+                    Dim arg_72_0 As Integer = num2
+                    Dim num3 As Integer = num
+                    If arg_72_0 > num3 Then
+                        Exit While
+                    End If
+                    bitmap.SetPixel(num2, 0, Color.FromArgb(CInt(array(num2)), 0, 0))
+                    num2 += 1
+                End While
+
+                bitmap.Save(saveFileDialog.FileName, ImageFormat.Png)
+                MsgBox(saveFileDialog.FileName, , "DONE!")
+            End If
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+End Class
